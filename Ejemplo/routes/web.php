7010943a4cc171ec131baecdeeb79c67acbfb994
Jocamo00 <jose.carreres.81@gmail.com>
2023-02-18 09:42:44 +0100

    toma de contacto con las rutas

--- Ejemplo/routes/web.php
+++ Ejemplo/routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{cadena?}', function ($cadena = null) {
+
+    $resultado = 'No conocido';
+    switch ($cadena) {
+        case 'hola-mundo':
+            $resultado = 'Hola usuario';
+            break;
+        case 'hola-laravel':
+            $resultado = 'Hola! Soy bastante jóven y tengo menos de un mes';
+            break;
+        default:
+            $resultado = 'Pedona, no te he entendido';
+            break;
+    }
+
+    return view('welcome', [
+        'cadena'=>$resultado
+    ]);
 });
+
+
+
